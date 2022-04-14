4ca38fd59b4ea15f8252ea5cbbb7cd7f00293838
root <root@kvmcore11.icic.boe>
2022-04-13 20:33:06 -0400

    ii

--- SimpleHttpServer.java
+++ SimpleHttpServer.java
@@ -12,7 +12,7 @@ import com.sun.net.httpserver.HttpServer;
 public class SimpleHttpServer {
 
   public static void main(String[] args) throws Exception {
-    HttpServer server = HttpServer.create(new InetSocketAddress(8081), 0);
+    HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
     server.createContext("/", new MyHandler());
     server.setExecutor(null); // creates a default executor
     server.start();
